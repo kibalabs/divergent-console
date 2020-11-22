0eef564cfd3d72617b2dd2e7df0e1a4636bd3724
Krishan Patel <krishan711@gmail.com>
2020-11-22 15:43:35 +0000

    .

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useFavicon } from '@kibalabs/core-react';
+import { useFavicon, Router, Route } from '@kibalabs/core-react';
 import { buildTheme, KibaApp, Text } from '@kibalabs/ui-react';
+import { EmptyPage } from './pages/emptyPage';
+import { DomainsPage } from './pages/domainsPage';
+import { DomainPage } from './pages/domainPage';
+import { NotFoundPage } from './pages/notFoundPage';
+import { CreateDomainPage } from './pages/createDomainPage';
+import { CreateLinkPage } from './pages/createLinkPage';
+import { LinkPage } from './pages/linkPage';
 
 const theme = buildTheme();
 
@@ -15,7 +22,15 @@ export const App = (): React.ReactElement => {
           <meta charSet='utf-8' />
           <title>Divergent Console</title>
         </Helmet>
-        <Text>Welcome to Divergent</Text>
+        <Router>
+          <Route path='/' page={EmptyPage} />
+          <Route path='/domains' page={DomainsPage}/>
+          <Route path='/domains/create' page={CreateDomainPage}/>
+          <Route path='/domains/:domainId' page={DomainPage} />
+          <Route path='/domains/:domainId/links/:linkId' page={LinkPage} />
+          <Route path='/domains/:domainId/links/create' page={CreateLinkPage} />
+          <Route default={true} page={NotFoundPage} />
+        </Router>
       </React.Fragment>
     </KibaApp>
   );
---
+++ app/src/pages/createLinkPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from '@kibalabs/ui-react';
+
+export interface ICreateLinkPageProps {
+}
+export const CreateLinkPage = (props: ICreateLinkPageProps): React.ReactElement => {
+  return (
+    <Text>Create Link</Text>
+  )
+}
